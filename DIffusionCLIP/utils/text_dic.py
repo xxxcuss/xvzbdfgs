6c965ec7053561bc179576f53258d5b5d9079594
xxxcuss <142451762+xxxcuss@users.noreply.github.com>
2025-12-30 15:22:05 -0500

    Add files via upload

---
+++ DIffusionCLIP/utils/text_dic.py
@@ -0,0 +1,188 @@
+SRC_TRG_TXT_DIC = {
+    # Human face
+    # --- Appearance --- 11
+    'tanned': (['face'], ['tanned face']),  # original
+    'pale': (['face'], ['pale face']),  # original
+    'makeup': (['person'], ['person with makeup']),  # original
+    'no_makeup': (['person'], ['person without makeup']),  # original
+    'old': (['person'], ['old person']),  # original
+    'young': (['person'], ['young person']),  # original
+    'beards': (['person'], ['person with beards']),  # original
+    'freckles': (['face'], ['face with freckles']),  # added
+    'wrinkles': (['face'], ['face with wrinkles']),  # added
+    'glasses': (['person'], ['person with glasses']),  # added
+    'bald': (['person'], ['bald person']),  # added
+
+    # --- Emotion/Expression --- 6
+    'angry': (['face'], ['angry face']),  # original
+    'surprised': (['face'], ['surprised face']),  # original
+    'smiling': (['face'], ['smiling face']),  # original
+    'crying': (['face'], ['crying face']),  # added
+    'laughing': (['face'], ['laughing face']),  # added
+    'serious': (['face'], ['serious face']),  # added
+
+    # --- Hair Color/Style --- 7
+    'blond_hair': (['person'], ['person with blond hair']),  # original
+    'red_hair': (['person'], ['person with red hair']),  # original
+    'grey_hair': (['person'], ['person with grey hair']),  # original
+    'curly_hair': (['person'], ['person with curly hair']),  # original
+    'black_hair': (['person'], ['person with black hair']),  # added
+    'straight_hair': (['person'], ['person with straight hair']),  # added
+    'ponytail': (['person'], ['person with ponytail']),  # added
+
+    # --- Celebrities/Identities --- 6
+    'nicolas': (['Person'], ['Nicolas Cage']),  # original
+    'zuckerberg': (['Person'], ['Mark Zuckerberg']),  # original
+    'benedict': (['Person'], ['Benedict Cumberbatch']),  # original
+    'elon_musk': (['Person'], ['Elon Musk']),  # added
+    'obama': (['Person'], ['Barack Obama']),  # added
+    'taylor_swift': (['Person'], ['Taylor Swift']),  # added
+
+    # --- Artistic Style --- 8
+    'gogh': (['photo'], ['painting by Gogh']),  # original
+    'frida': (['photo'], ['self-portrait by Frida Kahlo']),  # original
+    'modigliani': (['photo'], ['Painting in Modigliani style']),  # original
+    'sketch': (['photo'], ['sketch']),  # original
+    'watercolor': (['photo'], ['Watercolor Art with Thick Brushstrokes']),  # original
+    'cubism': (['photo'], ['cubism painting']),  # added
+    'pop_art': (['photo'], ['pop art style']),  # added
+    'anime': (['photo'], ['anime style illustration']),  # added
+
+    # --- Fantasy/Character Transformation --- 9
+    'elf': (['Human'], ['Tolkien elf']),  # original
+    'super_saiyan': (['Human'], ['Super saiyan']),  # original
+    'pixar': (['Human'], ['3D render in the style of Pixar']),  # original
+    'neanderthal': (['Human'], ['Neanderthal']),  # original
+    'zombie': (['Human'], ['Zombie']),  # original
+    'jocker': (['Human'], ['The Jocker']),  # original
+    'vampire': (['Human'], ['Vampire']),  # added
+    'werewolf': (['Human'], ['Werewolf']),  # added
+    'robot': (['Human'], ['Robot']),  # added
+
+    # Dog face
+    # === Character / Identity Manipulations ===
+    'dog_nicolas': (['Dog'], ['Nicolas Cage']),  # original
+    'dog_yorkshire': (['Dog'], ['Yorkshire Terrier']),  # original
+    'dog_smiling': (['Dog'], ['Smiling Dog']),  # original
+    'dog_zombie': (['Dog'], ['Zombie']),  # original
+    'dog_super_saiyan': (['Dog'], ['Super Saiyan']),  # original
+    'dog_venom': (['Dog'], ['Venom']),  # original
+    'dog_pikachu': (['Dog'], ['Pikachu']),  # added
+    'dog_spiderman': (['Dog'], ['Spiderman']),  # added
+    'dog_harry_potter': (['Dog'], ['Harry Potter']),  # added
+
+    # === Animal Hybrid Manipulations ===
+    'dog_bear': (['Dog'], ['Bear']),  # original
+    'dog_fox': (['Dog'], ['Fox']),  # original
+    'dog_wolf': (['Dog'], ['Wolf']),  # original
+    'dog_hamster': (['Dog'], ['Hamster']),  # original
+    'dog_cat': (['Dog'], ['Cat']),  # added
+    'dog_lion': (['Dog'], ['Lion']),  # added
+    'dog_tiger': (['Dog'], ['Tiger']),  # added
+    'dog_dragon': (['Dog'], ['Dragon']),  # added
+
+    # === Color Manipulations ===
+    'dog_red': (['Dog'], ['Red Dog']),  # added
+    'dog_blue': (['Dog'], ['Blue Dog']),  # added
+    'dog_green': (['Dog'], ['Green Dog']),  # added
+    'dog_purple': (['Dog'], ['Purple Dog']),  # added
+    'dog_golden': (['Dog'], ['Golden Dog']),  # added
+
+    # === Style Manipulations ===
+    'dog_cartoon': (['Dog'], ['Cartoon']),  # added
+    'dog_pixel_art': (['Dog'], ['Pixel Art']),  # added
+    'dog_cyberpunk': (['Dog'], ['Cyberpunk']),  # added
+    'dog_anime': (['Dog'], ['Anime']),  # added
+    'dog_low_poly': (['Dog'], ['Low Poly']),  # added
+    'dog_sketch': (['Dog'], ['Sketch']),  # added
+
+    # === Scene / Background Manipulations ===
+    'dog_angry': (['Dog'], ['Angry Dog']),  # added
+    'dog_sad': (['Dog'], ['Sad Dog']),  # added
+    'dog_sleeping': (['Dog'], ['Sleeping Dog']),  # added
+    'dog_running': (['Dog'], ['Running Dog']),  # added
+    'dog_jumping': (['Dog'], ['Jumping Dog']),  # added
+    'dog_tilting_head': (['Dog'], ['Dog Tilting Head']),  # added
+
+    #Church
+    # === Seasonal / Lighting Manipulations ===
+    'church_snow': (['Church'], ['Snow Covered Church']),  # original
+    'church_night': (['Church'], ['Church at Night']),  # original
+    'church_sunset': (['Church'], ['Church at Sunset']),  # added
+    'church_sunrise': (['Church'], ['Church at Sunrise']),  # added
+    'church_autumn': (['Church'], ['Autumn Leaves Church']),  # added
+    'church_spring': (['Church'], ['Spring Blossom Church']),  # added
+    'church_rainy': (['Church'], ['Church in Rain']),  # added
+
+    # === Material / Structure Manipulations ===
+    'church_red_brick': (['Church'], ['Red Brick Church']),  # original
+    'church_golden': (['Church'], ['Golden Church']),  # original
+    'church_wooden': (['Church'], ['Wooden Church']),  # original
+    'church_stone': (['Church'], ['Stone Church']),  # added
+    'church_marble': (['Church'], ['Marble Church']),  # added
+    'church_glass': (['Church'], ['Glass Church']),  # added
+    'church_iron': (['Church'], ['Iron Structure Church']),  # added
+
+    # === Style / Architecture Manipulations ===
+    'church_gothic': (['Church'], ['Gothic Church']),  # original
+    'church_ancient': (['Church'], ['Ancient Asian Tower']),  # original
+    'church_temple': (['Church'], ['Temple']),  # original
+    'church_renaissance': (['Church'], ['Renaissance Church']),  # added
+    'church_modern': (['Church'], ['Modern Church']),  # added
+    'church_baroque': (['Church'], ['Baroque Church']),  # added
+    'church_roman': (['Church'], ['Roman Style Church']),  # added
+    'church_minimalist': (['Church'], ['Minimalist Church']),  # added
+
+    # === Hybrid / Transformation Manipulations ===
+    'church_factory': (['Church'], ['Factory with Chimneys']),  # original
+    'church_department': (['Church'], ['Department Store']),  # original
+    'church_castle': (['Church'], ['Castle-like Church']),  # added
+    'church_mosque': (['Church'], ['Mosque-like Church']),  # added
+    'church_pagoda': (['Church'], ['Pagoda-style Church']),  # added
+    'church_library': (['Church'], ['Library Church']),  # added
+    'church_museum': (['Church'], ['Museum-like Church']),  # added
+    'church_skyscraper': (['Church'], ['Skyscraper Church']),  # added
+
+    #Bedroom
+    # === Color Manipulations ===
+    'bedroom_blue': (['Bedroom'], ['Blue tone Bedroom']),  # original
+    'bedroom_green': (['Bedroom'], ['Green tone Bedroom']),  # original
+    'bedroom_golden': (['Bedroom'], ['Golden Bedroom']),  # original
+    'bedroom_red': (['Bedroom'], ['Red tone Bedroom']),  # added
+    'bedroom_purple': (['Bedroom'], ['Purple tone Bedroom']),  # added
+    'bedroom_black': (['Bedroom'], ['Black tone Bedroom']),  # added
+    'bedroom_white': (['Bedroom'], ['White tone Bedroom']),  # added
+    'bedroom_pink': (['Bedroom'], ['Pink tone Bedroom']),  # added
+
+    # === Theme / Style Manipulations ===
+    'bedroom_princess': (['Bedroom'], ['Princess Bedroom']),  # original
+    'bedroom_palace': (['Bedroom'], ['Palace Bedroom']),  # original
+    'bedroom_modern': (['Bedroom'], ['Modern Bedroom']),  # added
+    'bedroom_minimalist': (['Bedroom'], ['Minimalist Bedroom']),  # added
+    'bedroom_vintage': (['Bedroom'], ['Vintage Bedroom']),  # added
+    'bedroom_japanese': (['Bedroom'], ['Japanese Style Bedroom']),  # added
+    'bedroom_cyberpunk': (['Bedroom'], ['Cyberpunk Bedroom']),  # added
+    'bedroom_royal': (['Bedroom'], ['Royal Bedroom']),  # added
+    'bedroom_scandinavian': (['Bedroom'], ['Scandinavian Style Bedroom']),  # added
+
+    # === Material / Furniture Manipulations ===
+    'bedroom_wooden': (['Bedroom'], ['Wooden Bedroom']),  # original
+    'bedroom_marble': (['Bedroom'], ['Marble Bedroom']),  # added
+    'bedroom_glass': (['Bedroom'], ['Glass Bedroom']),  # added
+    'bedroom_bamboo': (['Bedroom'], ['Bamboo Bedroom']),  # added
+    'bedroom_steel': (['Bedroom'], ['Steel Frame Bedroom']),  # added
+    'bedroom_leather': (['Bedroom'], ['Leather Furnished Bedroom']),  # added
+    'bedroom_fabric': (['Bedroom'], ['Fabric Style Bedroom']),  # added
+
+    # === Scene / Environment Manipulations ===
+    'bedroom_seaside': (['Bedroom'], ['Seaside Bedroom']),  # added
+    'bedroom_mountain': (['Bedroom'], ['Mountain View Bedroom']),  # added
+    'bedroom_garden': (['Bedroom'], ['Garden View Bedroom']),  # added
+    'bedroom_city': (['Bedroom'], ['City View Bedroom']),  # added
+    'bedroom_forest': (['Bedroom'], ['Forest Cabin Bedroom']),  # added
+    'bedroom_desert': (['Bedroom'], ['Desert Bedroom']),  # added
+    'bedroom_underwater': (['Bedroom'], ['Underwater Bedroom']),  # added
+    'bedroom_sky': (['Bedroom'], ['Sky Loft Bedroom']),  # added
+
+
+}
